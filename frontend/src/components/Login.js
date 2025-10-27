0ec4019decbef1ec257c8315bc96f7e1aa64567e
Umut Özcan <umutozcandev@gmail.com>
2025-10-27 10:29:25 +0300

    Remove duplicate imports in Login component
    Removed duplicate imports of useState and axios.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Login.css';
 
-import { useState } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
